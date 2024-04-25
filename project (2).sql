ff2844a4086e97bc2ff09dd2ea9def202259ff9e
Puravshah321 <124144406+Puravshah321@users.noreply.github.com>
2024-04-25 18:52:10 +0530

    project.sql

--- project (2).sql
+++ project (2).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2024 at 08:16 PM
+-- Generation Time: Apr 25, 2024 at 01:13 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `activity` (
-  `activity_id` varchar(110) NOT NULL,
-  `activity_name` varchar(110) NOT NULL,
-  `place` varchar(110) NOT NULL
+  `activity_id` varchar(6) NOT NULL,
+  `activity_name` varchar(150) NOT NULL,
+  `place` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -40,13 +40,11 @@ CREATE TABLE `activity` (
 --
 
 CREATE TABLE `booking` (
-  `booking_id` varchar(100) NOT NULL,
-  `group_id` varchar(100) NOT NULL,
+  `booking_id` varchar(6) NOT NULL,
   `booking_date` datetime NOT NULL,
   `customer_id` varchar(100) NOT NULL,
   `trip_id` varchar(100) NOT NULL,
-  `amount` decimal(10,0) NOT NULL,
-  `payment_type` varchar(100) NOT NULL,
+  `amount` decimal(20,0) NOT NULL,
   `status` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -57,9 +55,9 @@ CREATE TABLE `booking` (
 --
 
 CREATE TABLE `faq` (
-  `faq_id` varchar(110) NOT NULL,
-  `faq_question` varchar(250) NOT NULL,
-  `faq_answer` varchar(500) NOT NULL
+  `faq_id` varchar(6) NOT NULL,
+  `faq_question` varchar(100) NOT NULL,
+  `faq_answer` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -69,9 +67,9 @@ CREATE TABLE `faq` (
 --
 
 CREATE TABLE `feedback` (
-  `feedback_id` varchar(110) NOT NULL,
-  `booking_id` varchar(110) NOT NULL,
-  `customer_id` varchar(100) NOT NULL,
+  `feedback_id` varchar(6) NOT NULL,
+  `booking_id` varchar(6) NOT NULL,
+  `customer_id` varchar(6) NOT NULL,
   `remarks` varchar(400) NOT NULL,
   `rating` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -83,9 +81,9 @@ CREATE TABLE `feedback` (
 --
 
 CREATE TABLE `images` (
-  `sr_no` varchar(110) NOT NULL,
-  `image_id` varchar(110) NOT NULL,
-  `path_1` varchar(110) NOT NULL,
+  `sr_no` varchar(6) NOT NULL,
+  `image_id` varchar(6) NOT NULL,
+  `path_1` varchar(100) NOT NULL,
   `path_2` varchar(100) NOT NULL,
   `path_3` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -97,15 +95,15 @@ CREATE TABLE `images` (
 --
 
 CREATE TABLE `itinerary` (
-  `itinerary_id` varchar(100) NOT NULL,
+  `itinerary_id` varchar(6) NOT NULL,
+  `itinerary_title` varchar(100) NOT NULL,
   `day` int(11) NOT NULL,
-  `location_src` varchar(100) NOT NULL,
+  `location` varchar(100) NOT NULL,
   `distance` mediumint(9) NOT NULL,
-  `transport_means` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
   `start_time` time NOT NULL,
   `end_time` time NOT NULL,
-  `description` varchar(100) NOT NULL,
-  `activity_id` varchar(100) NOT NULL
+  `activity_id` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -115,25 +113,19 @@ CREATE TABLE `itinerary` (
 --
 
 CREATE TABLE `packages` (
-  `pack_id` varchar(100) NOT NULL,
-  `pack_type` varchar(100) NOT NULL,
-  `pack_name` varchar(100) NOT NULL,
-  `hotel_id` varchar(100) NOT NULL,
-  `pack_price` int(11) NOT NULL,
-  `itinerary_id` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `participant`
---
-
-CREATE TABLE `participant` (
-  `group_id` varchar(100) NOT NULL,
-  `participant_name` varchar(100) NOT NULL,
-  `DOB` datetime NOT NULL,
-  `guardian_phone_no` bigint(10) NOT NULL
+  `package_id` varchar(6) NOT NULL,
+  `package_type` varchar(100) NOT NULL,
+  `trip_type` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `grade` varchar(100) NOT NULL,
+  `season` varchar(100) NOT NULL,
+  `package_name` varchar(100) NOT NULL,
+  `duration` varchar(10) NOT NULL,
+  `date1` datetime NOT NULL,
+  `date2` datetime NOT NULL,
+  `date3` datetime NOT NULL,
+  `price` varchar(100) NOT NULL,
+  `itenerary_id` varchar(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -143,28 +135,58 @@ CREATE TABLE `participant` (
 --
 
 CREATE TABLE `registration` (
-  `customer_id` varchar(100) NOT NULL,
+  `institute_id` varchar(6) NOT NULL,
   `institute_name` varchar(100) NOT NULL,
-  `phone_no` bigint(20) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `address` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `confirm_password` varchar(100) NOT NULL
+  `address` varchar(500) NOT NULL,
+  `phone_number` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `registration`
+--
+
+INSERT INTO `registration` (`institute_id`, `institute_name`, `email`, `password`, `address`, `phone_number`) VALUES
+('I00001', 'abc', 'abc@gmail.com', 'abc', 'abc', '1234567890'),
+('I00002', 'nirman ', 'nirman@gmail.com', 'nirmna', 'vastrapur', '8019220948');
+
+--
+-- Triggers `registration`
+--
+DELIMITER $$
+CREATE TRIGGER `institute_id` BEFORE INSERT ON `registration` FOR EACH ROW BEGIN
+  DECLARE last_id INT;
+    DECLARE new_id VARCHAR(15);
+    
+    -- Get the maximum institute_id from the registration table
+    SELECT MAX(CAST(SUBSTRING(institute_id, 2) AS UNSIGNED)) INTO last_id FROM registration;
+    
+    -- If no records are present, initialize last_id to 0
+    IF last_id IS NULL THEN
+        SET last_id = 0;
+    END IF;
+    
+    -- Generate the new institute_id
+    SET new_id = CONCAT("I", LPAD(last_id + 1, 5, "0"));
+    
+    -- Set the new institute_id for the current insertion
+    SET NEW.institute_id = new_id;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `trip`
+-- Table structure for table `student`
 --
 
-CREATE TABLE `trip` (
-  `trip_id` varchar(100) NOT NULL,
-  `from_date` datetime NOT NULL,
-  `to_date` datetime NOT NULL,
-  `trip_type` varchar(100) NOT NULL,
-  `pack_id` varchar(100) NOT NULL,
-  `near_city` varchar(100) NOT NULL
+CREATE TABLE `student` (
+  `stu_id` varchar(6) NOT NULL,
+  `stu_name` varchar(100) NOT NULL,
+  `age` int(11) NOT NULL,
+  `guardian_num` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -181,102 +203,7 @@ ALTER TABLE `activity`
 -- Indexes for table `booking`
 --
 ALTER TABLE `booking`
-  ADD PRIMARY KEY (`booking_id`),
-  ADD KEY `customer_id` (`customer_id`),
-  ADD KEY `trip_id` (`trip_id`),
-  ADD KEY `group_id` (`group_id`);
-
---
--- Indexes for table `faq`
---
-ALTER TABLE `faq`
-  ADD PRIMARY KEY (`faq_id`);
-
---
--- Indexes for table `feedback`
---
-ALTER TABLE `feedback`
-  ADD PRIMARY KEY (`feedback_id`),
-  ADD KEY `booking_id` (`booking_id`),
-  ADD KEY `customer_id` (`customer_id`);
-
---
--- Indexes for table `images`
---
-ALTER TABLE `images`
-  ADD PRIMARY KEY (`sr_no`,`image_id`);
-
---
--- Indexes for table `itinerary`
---
-ALTER TABLE `itinerary`
-  ADD PRIMARY KEY (`itinerary_id`),
-  ADD KEY `activity_id` (`activity_id`);
-
---
--- Indexes for table `packages`
---
-ALTER TABLE `packages`
-  ADD PRIMARY KEY (`pack_id`),
-  ADD KEY `itinerary_id` (`itinerary_id`);
-
---
--- Indexes for table `participant`
---
-ALTER TABLE `participant`
-  ADD PRIMARY KEY (`group_id`);
-
---
--- Indexes for table `registration`
---
-ALTER TABLE `registration`
-  ADD PRIMARY KEY (`customer_id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `phone_no` (`phone_no`);
-
---
--- Indexes for table `trip`
---
-ALTER TABLE `trip`
-  ADD PRIMARY KEY (`trip_id`),
-  ADD KEY `pack_id` (`pack_id`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `booking`
---
-ALTER TABLE `booking`
-  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `registration` (`customer_id`),
-  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`trip_id`) REFERENCES `trip` (`trip_id`),
-  ADD CONSTRAINT `booking_ibfk_3` FOREIGN KEY (`group_id`) REFERENCES `participant` (`group_id`);
-
---
--- Constraints for table `feedback`
---
-ALTER TABLE `feedback`
-  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`booking_id`),
-  ADD CONSTRAINT `feedback_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `registration` (`customer_id`);
-
---
--- Constraints for table `itinerary`
---
-ALTER TABLE `itinerary`
-  ADD CONSTRAINT `itinerary_ibfk_1` FOREIGN KEY (`activity_id`) REFERENCES `activity` (`activity_id`);
-
---
--- Constraints for table `packages`
---
-ALTER TABLE `packages`
-  ADD CONSTRAINT `packages_ibfk_1` FOREIGN KEY (`itinerary_id`) REFERENCES `itinerary` (`itinerary_id`);
-
---
--- Constraints for table `trip`
---
-ALTER TABLE `trip`
-  ADD CONSTRAINT `trip_ibfk_1` FOREIGN KEY (`pack_id`) REFERENCES `packages` (`pack_id`);
+  ADD PRIMARY KEY (`booking_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
